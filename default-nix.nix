3803ef52e2d2ab4232f5ccf8c11bfb38ef15f850
Dan Dart <git@dandart.co.uk>
2025-08-13 02:11:45 +0100

    lint

--- default-nix.nix
+++ default-nix.nix
@@ -1,15 +1,15 @@
 {
   nixpkgs ? import <nixpkgs> {},
   haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {
-    nixpkgs = nixpkgs;
-    compiler = compiler;
+    inherit nixpkgs;
+    inherit compiler;
   },
   compiler ? "ghc912"
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   tools = haskell-tools compiler;
-  lib = nixpkgs.pkgs.haskell.lib;
+  inherit (nixpkgs.pkgs.haskell) lib;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       granular-io = lib.dontHaddock (self.callCabal2nix "granular-io" (gitignore ./.) {});
@@ -25,7 +25,7 @@ let
     ];
     shellHook = ''
       gen-hie > hie.yaml
-      for i in $(find -type f | grep -v "dist-*"); do krank $i; done
+      for i in $(find . -type f | grep -v "dist-*"); do krank $i; done
     '';
     buildInputs = tools.defaultBuildTools;
     withHoogle = false;
@@ -33,5 +33,5 @@ let
   in
 {
   inherit shell;
-  granular-io = lib.justStaticExecutables (myHaskellPackages.granular-io);
+  granular-io = lib.justStaticExecutables myHaskellPackages.granular-io;
 }
